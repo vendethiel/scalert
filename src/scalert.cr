b2b791598d23e009c2615e35ce7227b8315762ce
vendethiel <vendethiel@hotmail.fr>
2018-03-08 16:51:21 +0100

    Use IDs, allow many different types
    Fixes #7
    Fixes #5
    Fixes #4, though... only partly. I'll make another issue of that, or
    probably: reuse #3.

--- src/scalert.cr
+++ src/scalert.cr
@@ -5,6 +5,8 @@ require "./scalert/*"
 require "../lib/discordcr/src/discordcr" # include dep
 require "../lib/discordcr/src/discordcr/*" # include dep
 
+API_BASE_URL = "http://elgrandeapidelteamliquid.herokuapp.com"
+
 class Alias
   def initialize(@filename : String)
     @aliases = {} of String => String
@@ -19,7 +21,7 @@ class Alias
     end
   end
 
-  delegate :fetch, to: @aliases
+  delegate :fetch, :[], :has_key?, to: @aliases
   def []=(key, value)
     @aliases[key] = value
     save!
@@ -35,14 +37,16 @@ class Alias
 end
 
 class ScEvent
+  getter id : Int64
   getter name : String
   getter desc : String
+  getter game : String
 
-  def initialize(@name : String, @desc : String)
+  def initialize(@id, @name, @desc, @game)
   end
 
-  def to_s
-    " * #{name} – #{desc}"
+  def to_s(include_game = false)
+    " * #{name}#{include_game ? " (#{@game})" : ""} – #{desc}"
   end
 
   def self.json_name(event)
@@ -50,25 +54,45 @@ class ScEvent
   end
 
   def self.json_desc(event, urls)
-    if event["url"]?
-      name = event["name"].as_s
+    name = event["name"].as_s
+    if event["timer"]?
+      if event["timer"].as_s.includes?("d") # exclude >=1d
+        nil
+      else
+        event["timer"].as_s
+      end
+    elsif event["url"]?
       "<#{urls.fetch(name, event["url"].as_s)}>"
-    elsif event["timer"].as_s.includes?("d") # exclude >=1d
-      nil
+    elsif urls.has_key?(name)
+      "<#{urls[name]}>"
     else
-      event["timer"].as_s
+      "(no url)"
     end
   end
 end
 
 class ScAlert
-  CHANNEL_ID = 328555540831666178_u64
+  ANNOUNCEMENTS = {
+    328555540831666178_u64 => %w(sc2 bw)  # Test server, #general
+    306615995466776586_u64 => %w(bw)      # FBW #announcements
+  }
+  EVENTS_COMMAND = {
+    328555540831666178_u64 => %w(sc2 bw), # Test server #general
+    421332119118151680_u64 => %w(sc2)     # Test server #command-events-sc2
+    306259732153368576_u64 => %w(bw)      # FBW #general
+  }
   ADMINS = [
-    116306741058207744_u64 # Ven
+    116306741058207744_u64, # Ven
+    133280548951949312_u64, # Kuro
+    117555772560244739_u64  # Light
   ]
 
   def initialize(@client : Discord::Client, alias_filename : String)
     @aliases = Alias.new(alias_filename)
+  end
+
+  def format_events(events, show_game)
+    events.map{|e| e.to_s(show_game)}.join("\n")
   end
 
   def poll_events
@@ -84,12 +108,18 @@ class ScAlert
         new_events = events.reject{|e| current_events.includes?(e.name) }
         puts("new events: #{new_events}")
         if new_events.size > 0
-          fnew_events = new_events.map &.to_s
-          @client.create_message(CHANNEL_ID, " ** LIVE **\n" + fnew_events.join("\n"))
+          # for each channel to announce on...
+          ANNOUNCEMENTS.each do |channel_id, games|
+            events_to_announce = events.select{|e| games.includes?(e.game)}
+            next unless events_to_announce.size > 0
+            show_game = games.size > 1 # show the game if there could be confusion
+            @client.create_message(channel_id, " ** LIVE **\n" + format_events(events_to_announce, show_game))
+          end
         end
 
         # update the events list
-        current_events = events.map {|e| e.name}
+        current_events += new_events.map(&.id)
+        # TODO clear old events SOMEHOW (timer? only keep N latest IDs?)
       rescue e
         puts "Rescued exception\n#{e.message}"
       end
@@ -99,15 +129,14 @@ class ScAlert
 
   def map_events(events)
     events.compact_map do |event|
-      next if event["game"].as_s != "sc2"
       desc = ScEvent.json_desc(event, @aliases)
       next unless desc
-      ScEvent.new(ScEvent.json_name(event), desc)
+      ScEvent.new(event["id"].as_s.to_i64, ScEvent.json_name(event), desc, event["game"].as_s)
     end
   end
 
   def fetch_category(name)
-    response = HTTP::Client.get "http://elgrandeapidelteamliquid.herokuapp.com/#{name}"
+    response = HTTP::Client.get "#{API_BASE_URL}/#{name}"
     if response.status_code != 200
       return nil
     end
@@ -142,20 +171,14 @@ class ScAlert
   end
 
   def command_events(payload)
-    levents = run_category("levents")
-    if levents && levents.size > 0
-      flevents = levents.map &.to_s
-      @client.create_message(payload.channel_id, " ** LIVE **\n" + flevents.join("\n"))
-    else
-      puts("no levents")
-    end
-
-    uevents = run_category("uevents")
-    if uevents && uevents.size > 0
-      fuevents = uevents.map &.to_s
-      @client.create_message(payload.channel_id, " ** UPCOMING **\n" + fuevents.join("\n"))
-    else
-      puts("no uevents")
+    return unless EVENTS_COMMAND.has_key?(payload.channel_id)
+    games = EVENTS_COMMAND[payload.channel_id]
+    show_game = games.size > 1 # show the game if there could be confusion
+    {"levents" => "LIVE", "uevents" => "UPCOMING"}.each do |category, label|
+      events = run_category(category)
+      events_to_announce = events && events.select{|e| games.includes?(e.game)}
+      next unless events_to_announce && events_to_announce.size > 0
+      @client.create_message(payload.channel_id, " ** #{label} **\n" + format_events(events_to_announce, show_game))
     end
   end
 end
